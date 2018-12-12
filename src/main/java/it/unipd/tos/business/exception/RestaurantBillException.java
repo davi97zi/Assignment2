e6997f37dd7b4a468ea9cdf5eb5d91c695dfeeb4
Davide Zilio <ziliodav97@gmail.com>
2018-12-12 12:01:34 +0100

    Update RestaurantBillException.java

--- src/main/java/it/unipd/tos/business/exception/RestaurantBillException.java
+++ src/main/java/it/unipd/tos/business/exception/RestaurantBillException.java
@@ -6,8 +6,8 @@ package it.unipd.tos.business.exception;
 
 public class RestaurantBillException extends Exception {
     
-    public RestaurantBillException(String m){
-        super(m);
+    public RestaurantBillException(String message){
+        super(message);
     }
     
     public String getMessage(){
