8973ae89b7c475d61a56864dea84c9a78522e8f7
Davide Zilio <ziliodav97@gmail.com>
2018-12-12 16:38:22 +0100

    Update Bill.java

--- src/main/java/it/unipd/tos/business/Bill.java
+++ src/main/java/it/unipd/tos/business/Bill.java
@@ -11,10 +11,11 @@ import it.unipd.tos.model.MenuItem.itemType;
 
 public class Bill implements RestaurantBill {
   public double getOrderPrice(List<MenuItem> l) throws RestaurantBillException { 
-    if(l.size() > 20) {
-      throw new RestaurantBillException("Numero di ordinazioni" + 
-                     " maggiori di 20");
-    } 
+    
+    if(l.size()>20) {
+      throw new RestaurantBillException("Errore: ordinazione troppo grande");  
+    }  
+    
     double price=0;
     for(MenuItem i : l) { //calcolo somma
       price+=i.getPrezzo();
